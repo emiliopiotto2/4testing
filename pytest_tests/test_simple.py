f611cdaa94668e29e0980619ed4206913cee0ec1
emilio.piotto <emilio.piotto@accenture.com>
2022-03-14 11:24:02 -0300

    change test to pass

--- pytest_tests/test_simple.py
+++ pytest_tests/test_simple.py
@@ -2,7 +2,7 @@ def test_passed():
     '''Hoping to see the action running'''
     assert (True == True)
     
-a = False
+a = True
 def test_chengeimpact():
     '''Hoping to see the action running'''
     assert (True == a)
