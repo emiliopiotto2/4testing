500fa073ef68ce00c0c6aa573342f8d904dcf76f
emilio.piotto <emilio.piotto@accenture.com>
2022-03-14 11:16:11 -0300

    new test should fail

--- pytest_tests/test_simple.py
+++ pytest_tests/test_simple.py
@@ -1,3 +1,8 @@
 def test_passed():
     '''Hoping to see the action running'''
     assert (True == True)
+    
+a = False
+def test_chengeimpact():
+    '''Hoping to see the action running'''
+    assert (True == a)
